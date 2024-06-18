238d86c2bb0266375e838fc77311baf4961ff41d
Kevin Netherton <kevin.netherton@gov.bc.ca>
2024-06-18 10:30:25 -0700

    fix: tweaks to the typing required by upgrade to pydantic v2

--- backend-py/src/core/config.py
+++ backend-py/src/core/config.py
@@ -1,23 +1,25 @@
 import os
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union
 
 from pydantic import field_validator, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+
     API_V1_STR: str = "/api/v1"
     POSTGRES_SERVER: str = os.getenv("POSTGRES_HOST", "127.0.0.1:5432")
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "postgres")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "postgres")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "postgres")
-    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = PostgresDsn.build(
+    SQLALCHEMY_DATABASE_URI: Union[Optional[PostgresDsn], Optional[str]] = PostgresDsn.build(
         scheme="postgresql",
-        user=os.getenv("POSTGRES_USER", "postgres"),
+        username=os.getenv("POSTGRES_USER", "postgres"),
         password=os.getenv("POSTGRES_PASSWORD", "postgres"),
         host=os.getenv("POSTGRES_HOST", "127.0.0.1:5432"),
         path=f"/{os.getenv('POSTGRES_DB', 'postgres')}",
     )
+    
 
 
 @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
@@ -27,7 +29,7 @@ def assemble_db_connection(v: Optional[str], values: Dict[str, Any]) -> Any:
         return v
     return PostgresDsn.build(
         scheme="postgresql",
-        user=values.get("POSTGRES_USER"),
+        username=values.get("POSTGRES_USER"),
         password=values.get("POSTGRES_PASSWORD"),
         host=values.get("POSTGRES_SERVER"),
         path=f"/{values.get('POSTGRES_DB') or ''}",
