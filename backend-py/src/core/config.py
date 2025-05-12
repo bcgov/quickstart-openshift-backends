9c35fdcc2e3232cf4076136f9c4994697f89371c
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-05-11 22:27:56 -0700

    fix: config and secret for python

--- backend-py/src/core/config.py
+++ backend-py/src/core/config.py
@@ -1,36 +1,29 @@
 import os
-from typing import Any, Dict, Optional, Union
 from urllib.parse import quote
-from pydantic import field_validator, PostgresDsn
-from pydantic_settings import BaseSettings
+from typing import Optional, Union, Dict, Any
+from pydantic import PostgresDsn, BaseSettings, field_validator
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_HOST", "127.0.0.1:5432")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "127.0.0.1")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "5432")
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "postgres")
     POSTGRES_PASSWORD: str = quote(os.getenv("POSTGRES_PASSWORD", "postgres"), safe='')
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "postgres")
-    SQLALCHEMY_DATABASE_URI: Union[Optional[PostgresDsn], Optional[str]] = PostgresDsn.build(
-        scheme="postgresql",
-        username=os.getenv("POSTGRES_USER", "postgres"),
-        password=quote(os.getenv("POSTGRES_PASSWORD", "postgres"), safe=''),
-        host=os.getenv("POSTGRES_HOST", "127.0.0.1:5432"),
-        path=f"{os.getenv('POSTGRES_DB', 'postgres')}",
-    )
+    SQLALCHEMY_DATABASE_URI: Union[Optional[PostgresDsn], Optional[str]]
 
-
-@field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
-@classmethod
-def assemble_db_connection(v: Optional[str], values: Dict[str, Any]) -> Any:
-    if isinstance(v, str):
-        return v
-    return PostgresDsn.build(
-        scheme="postgresql",
-        username=values.get("POSTGRES_USER"),
-        password=values.get("POSTGRES_PASSWORD"),
-        host=values.get("POSTGRES_SERVER"),
-        path=f"{values.get('POSTGRES_DB') or ''}",
-    )
-
+    @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
+    @classmethod
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            username=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOST"),
+            port=int(values.get("POSTGRES_PORT")),
+            path=f"{values.get('POSTGRES_DB') or ''}",
+        )
 
 Configuration = Settings()
